b800a858a28b74dcc703f104bb3ecaccb896e0ab
fairever <git@fairever.aleeas.com>
2026-07-17 08:59:12 +0800

    fix: typo

--- tests/signin.spec.ts
+++ tests/signin.spec.ts
@@ -1,13 +1,13 @@
-import { test, expect } from 'patchright/test';
-
+import { test, expect } from 'patchright/test';
+
 const HOME_PAGE = "https://bbs.yamibo.com/plugin.php?id=zqlj_sign"
 
 test('signin', async ({ page }) => {
-  await page.goto(HOME_PAGE);
-
-  await page.getByRole('link', { name: '点击打卡'}).click();
+  await page.goto(HOME_PAGE);
+
+  await page.getByRole('link', { name: '点击打卡'}).click();
 
   await new Promise(resolve => setTimeout(resolve, 10000));
-
-  await expect(page.getByRole('link', { name: '今日已打卡'})).toBeVisiable();
+
+  await expect(page.getByRole('link', { name: '今日已打卡'})).toBeVisible();
 });
